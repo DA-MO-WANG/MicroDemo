13252914369c94d008062f08db3ed9d20b84cc69
han guo <guohan741@163.com>
2021-08-16 18:35:00 +0800

    commit demo

--- java/aopdemo/src/main/java/com/example/aopdemo/annotation/MetricAspect.java
+++ java/aopdemo/src/main/java/com/example/aopdemo/annotation/MetricAspect.java
@@ -5,11 +5,14 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+/**
+ * 切面整合类
+ */
 @Aspect
 @Component
 public class MetricAspect {
     //基于注解定位的切面注入点
-    @Around("@annotation(metricTime)")
+    @Around("@annotation(metricTime)")//指定注解类型@annotation
     public Object metric(ProceedingJoinPoint joinPoint, MetricTime metricTime) throws Throwable {
         String name = metricTime.value();
         long start = System.currentTimeMillis();
