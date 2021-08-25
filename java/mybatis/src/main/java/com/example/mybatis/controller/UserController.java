2a8446d9eecee1f048233ddc9a5d6b6967b856fa
han guo <guohan741@163.com>
2021-08-25 14:27:00 +0800

    commit demo

--- java/mybatis/src/main/java/com/example/mybatis/controller/UserController.java
+++ java/mybatis/src/main/java/com/example/mybatis/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.mybatis.controller;
 
 import com.example.mybatis.db.UserDao;
+import com.example.mybatis.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ public class UserController {
 
     @GetMapping
     public String hello() {
-        userDao.searchUser("")
+        User user = userDao.searchUser("mike");
+        return user.getScore()+ "";
     }
 }
