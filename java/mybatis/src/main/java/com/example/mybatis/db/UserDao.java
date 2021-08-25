bdf788a3e9a5d96abdaf081c090e513dcbc9008a
han guo <guohan741@163.com>
2021-08-25 14:30:00 +0800

    commit demo

--- java/mybatis/src/main/java/com/example/mybatis/db/UserDao.java
+++ java/mybatis/src/main/java/com/example/mybatis/db/UserDao.java
@@ -3,10 +3,12 @@ package com.example.mybatis.db;
 import com.example.mybatis.pojo.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
 
 @Mapper
+@Repository
 public interface UserDao {
     @Select("select * from user where name=#{name}")
     User searchUser(String name);
