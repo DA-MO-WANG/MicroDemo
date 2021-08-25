df7ce53f9adc0dc5b584726478a2749b2bd912bd
han guo <guohan741@163.com>
2021-08-25 15:32:17 +0800

    commit demo

--- java/mybatis/src/main/java/com/example/mybatis/MybatisApplication.java
+++ java/mybatis/src/main/java/com/example/mybatis/MybatisApplication.java
@@ -13,6 +13,7 @@ public class MybatisApplication {
     /*引入依赖合适，都不用自己配数据库的config，直接面向yml编程，把需要配置的参数都放到yml里
     * ----mybatis的starter自动配置的作用，省去了 config、bean
     *
+    * namespace--mapper中作用是绑定对应的dao接口
     *
     * */
 
