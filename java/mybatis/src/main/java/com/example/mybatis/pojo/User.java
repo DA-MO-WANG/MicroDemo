aa16b66b4ed283c10c50a28b8974f7a447f96476
han guo <guohan741@163.com>
2021-08-25 11:01:38 +0800

    commit demo

--- java/mybatis/src/main/java/com/example/mybatis/pojo/User.java
+++ java/mybatis/src/main/java/com/example/mybatis/pojo/User.java
@@ -1,2 +1,10 @@
-package com.example.mybatis.pojo;public class User {
+package com.example.mybatis.pojo;
+
+import lombok.Data;
+
+@Data
+public class User {
+    public int id;
+    public String name;
+    public int score;
 }
