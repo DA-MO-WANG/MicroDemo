539aa31343590f34ca80a148300bc14c18d30a6f
han guo <guohan741@163.com>
2021-08-25 15:20:00 +0800

    commit demo

--- java/mybatis/src/main/java/com/example/mybatis/MybatisApplication.java
+++ java/mybatis/src/main/java/com/example/mybatis/MybatisApplication.java
@@ -9,5 +9,11 @@ public class MybatisApplication {
     public static void main(String[] args) {
         SpringApplication.run(MybatisApplication.class, args);
     }
+    //1.搭建过程中的感悟
+    /*引入依赖合适，都不用自己配数据库的config，直接面向yml编程，把需要配置的参数都放到yml里
+    * ----mybatis的starter自动配置的作用，省去了 config、bean
+    *
+    *
+    * */
 
 }
