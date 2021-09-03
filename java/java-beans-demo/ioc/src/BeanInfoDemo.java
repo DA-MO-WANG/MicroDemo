121d2b78fd3cda72e57fa3e1544875a604435831
han guo <guohan741@163.com>
2021-09-03 13:08:00 +0800

    commit demo

--- java/java-beans-demo/ioc/src/BeanInfoDemo.java
+++ java/java-beans-demo/ioc/src/BeanInfoDemo.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class BeanInfo {
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.util.stream.Stream;
+
+/**
+ * {@link java.beans.BeanInfo} 示例
+ */
+public class BeanInfoDemo {
+    public static void main(String[] args) throws IntrospectionException {
+        BeanInfo beanInfo = Introspector.getBeanInfo(Person.class);
+        Stream.of(beanInfo.getPropertyDescriptors())
+                .forEach(propertyDescriptor -> {
+                    System.out.println(propertyDescriptor);
+                });
+
+
+
+    }
 }
--- java/java-beans-demo/ioc/src/Person.java
+++ java/java-beans-demo/ioc/src/Person.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Person {
+/**
+ * java bean如何看待这个类，只分两种：可写、可读
+ * 它只有
+ */
+public class Person {
+    String name;
+    Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
