47e954f8a75ef4ec774dcfc7b361fb7f7312c710
han guo <guohan741@163.com>
2021-09-08 17:54:01 +0800

    commit demo

--- java/transactiontest/src/main/java/com/example/transactiontest/TransactiontestApplication.java
+++ java/transactiontest/src/main/java/com/example/transactiontest/TransactiontestApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TransactiontestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TransactiontestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TransactiontestApplication.class, args);
+    }
 
 }
