31a27bf9a4ba01689e1da97464e1ffb87aa64d0e
han guo <guohan741@163.com>
2021-09-10 23:05:00 +0800

    commit demo

--- java/mirojava/src/suo/buzhengquefangwenziyuan/EvenGenerator.java
+++ java/mirojava/src/suo/buzhengquefangwenziyuan/EvenGenerator.java
@@ -1,2 +1,14 @@
-package suo.buzhengquefangwenziyuan;public class EvenGenerator {
+package suo.buzhengquefangwenziyuan;
+
+public class EvenGenerator extends IntGenerator{
+    private int currentEvenValue = 0;
+    @Override
+    public int next() {
+        ++currentEvenValue;
+        ++currentEvenValue;
+        return currentEvenValue;
+    }
+    public static void main(String[] args) {
+        EvenChecker.test(new EvenGenerator());
+    }
 }
