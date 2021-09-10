f0d8b7499bee9c37f0b562c7bc0a406893bf17a0
han guo <guohan741@163.com>
2021-09-10 14:38:01 +0800

    commit demo

--- java/mirojava/src/suo/buzhengquefangwenziyuan/IntGenerator.java
+++ java/mirojava/src/suo/buzhengquefangwenziyuan/IntGenerator.java
@@ -1,2 +1,14 @@
-package suo.buzhengquefangwenziyuan;public class IntGenerator {
+package suo.buzhengquefangwenziyuan;
+
+import javax.print.DocFlavor;
+
+public abstract class IntGenerator {
+    private volatile boolean canceled = false;
+    public abstract int next();
+    public void cancle() {
+        canceled = true;
+    }
+    public boolean isCanceled() {
+        return canceled;
+    }
 }
