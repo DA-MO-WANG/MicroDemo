8bc7307d98c5c642242c747e4a3da0fb3761bb74
han guo <guohan741@163.com>
2021-09-11 17:36:00 +0800

    commit demo

--- java/mirojava/src/suo/buzhengquefangwenziyuan/Task.java
+++ java/mirojava/src/suo/buzhengquefangwenziyuan/Task.java
@@ -15,6 +15,7 @@ public class Task implements Runnable{
     public void run() {
         user.live();
         if (user.age % 2 != 0) {
+            //共享同一片内存，有写操作，有读操作--读写操作，多线程下就出问题
             System.out.println("并发失败--"+user.age+"-"+id);
         }
     }
