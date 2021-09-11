c6baef509f2edfa9a81316d0679c258a3645dccf
han guo <guohan741@163.com>
2021-09-11 13:42:00 +0800

    commit demo

--- java/mirojava/src/suo/buzhengquefangwenziyuan/User.java
+++ java/mirojava/src/suo/buzhengquefangwenziyuan/User.java
@@ -1,12 +1,15 @@
 package suo.buzhengquefangwenziyuan;
 
 public class User {
-    int age;
-    int score;
+    int age = 0;
+    int score = 2;
 
     public User(int age, int score) {
         this.age = age;
         this.score = score;
+    }
+
+    public User() {
     }
 
     public void live() {
