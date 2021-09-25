a7018c5bc38fae131bf70e002263d96a102b7df6
han guo <guohan741@163.com>
2021-09-25 11:07:00 +0800

    commit demo

--- java/mirojava/src/jvm/ReferenceCountingGC.java
+++ java/mirojava/src/jvm/ReferenceCountingGC.java
@@ -1,2 +1,22 @@
-package jvm;public class ReferenceCountingGC {
+package jvm;
+
+/**
+ * testGC() 方法执行后，objA objB 会不会被gc呢？
+ * @author zzm
+ */
+public class ReferenceCountingGC {
+    public Object instance = null;
+    private static final int _1MB = 1024 * 1024;
+
+    private byte[] bigSize = new byte[2 * _1MB];
+    public static void main(String[] args) {
+        ReferenceCountingGC objA = new ReferenceCountingGC();
+        ReferenceCountingGC objB = new ReferenceCountingGC();
+        objA.instance = objB;
+        objB.instance = objA;
+
+        objA = null;
+        objB = null;
+        System.gc();
+    }
 }
