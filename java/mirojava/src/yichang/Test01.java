3e3a2fe7c493ab7496c2e6db091a62f7fcbc7df1
han guo <guohan741@163.com>
2021-10-26 18:09:00 +0800

    commit demo

--- java/mirojava/src/yichang/Test01.java
+++ java/mirojava/src/yichang/Test01.java
@@ -1,13 +1,24 @@
 package yichang;
 
 public class Test01 {
+    public void print() {
+        System.out.println("捕捉空指针异常");
+    }
     public static void main(String[] args) {
-        int x = 1;
+       /* int x = 1;
         int j = 0;
         try {
             int m = x /j;
         }catch (ArithmeticException e) {
             System.out.println("能够捕捉");
+        }*/
+
+        Test01 test01 = null;
+        try{
+            test01.print();
+        }catch (NullPointerException e) {
+            System.out.println("------------");
         }
+
     }
 }
