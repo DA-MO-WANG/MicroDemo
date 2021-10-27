245ed7676abd39bf87f464648a3632b931ed7bea
han guo <guohan741@163.com>
2021-10-27 18:06:00 +0800

    commit demo

--- java/mirojava/src/yichang/Test01.java
+++ java/mirojava/src/yichang/Test01.java
@@ -1,10 +1,14 @@
 package yichang;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Test01 {
     public void print() {
         System.out.println("捕捉空指针异常");
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
        /* int x = 1;
         int j = 0;
         try {
@@ -13,12 +17,16 @@ public class Test01 {
             System.out.println("能够捕捉");
         }*/
 
-        Test01 test01 = null;
+        /*Test01 test01 = null;
         try{
             test01.print();
         }catch (NullPointerException e) {
             System.out.println("------------");
-        }
+        }*/
+        String path = null;
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+                return br.readLine();
+            }
 
     }
 }
