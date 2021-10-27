0d4b92daaa4d405562164009ebb371cc860c5e50
han guo <guohan741@163.com>
2021-10-27 18:16:00 +0800

    commit demo

--- java/mirojava/src/yichang/Test01.java
+++ java/mirojava/src/yichang/Test01.java
@@ -1,5 +1,7 @@
 package yichang;
 
+import com.sun.codemodel.internal.JCatchBlock;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -28,8 +30,13 @@ public class Test01 {
     }
     public String get() throws IOException {
         String path = null;
+        String str = null;
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            System.out.println(str.length());
             return br.readLine();
+            
+        }catch (IOException |NullPointerException e) {
+            System.out.println("111111");
         }
     }
 }
