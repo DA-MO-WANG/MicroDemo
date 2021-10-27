f9bc90fbf4da6a7b7d88090b491610ab232e0aff
han guo <guohan741@163.com>
2021-10-27 18:07:00 +0800

    commit demo

--- java/mirojava/src/yichang/Test01.java
+++ java/mirojava/src/yichang/Test01.java
@@ -17,16 +17,19 @@ public class Test01 {
             System.out.println("能够捕捉");
         }*/
 
-        /*Test01 test01 = null;
-        try{
+        Test01 test01 = null;
+        /*try{
             test01.print();
         }catch (NullPointerException e) {
             System.out.println("------------");
         }*/
+        test01.print();
+
+    }
+    public String get() throws IOException {
         String path = null;
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-                return br.readLine();
-            }
-
+            return br.readLine();
+        }
     }
 }
