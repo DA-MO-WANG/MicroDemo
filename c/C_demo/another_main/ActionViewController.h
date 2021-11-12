b835358215bd13a132d5d570fa6d34d5fb3cf6f5
han guo <guohan741@163.com>
2021-11-12 21:10:00 +0800

    commit demo

---
+++ c/C_demo/another_main/ActionViewController.h
@@ -0,0 +1,12 @@
+//
+//  ActionViewController.h
+//  another_main
+//
+//  Created by rich heart on 2021/11/12.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ActionViewController : NSViewController
+
+@end
