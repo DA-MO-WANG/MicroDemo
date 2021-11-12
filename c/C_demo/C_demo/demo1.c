223d9c7e437265b2fb55c2973dbe6e142f69fd35
han guo <guohan741@163.com>
2021-11-12 19:53:00 +0800

    commit demo

---
+++ c/C_demo/C_demo/demo1.c
@@ -0,0 +1,8 @@
+//
+//  demo1.c
+//  C_demo
+//
+//  Created by rich heart on 2021/11/12.
+//
+
+#include "demo1.h"
---
+++ c/C_demo/C_demo/demo1.h
@@ -0,0 +1,13 @@
+//
+//  demo1.h
+//  C_demo
+//
+//  Created by rich heart on 2021/11/12.
+//
+
+#ifndef demo1_h
+#define demo1_h
+
+#include <stdio.h>
+
+#endif /* demo1_h */
