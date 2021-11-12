4246282792070e6a843cbeab54758e597e193180
han guo <guohan741@163.com>
2021-11-12 19:55:00 +0800

    commit demo

--- c/C_demo/C_demo/demo1.c
+++ c/C_demo/C_demo/demo1.c
@@ -6,3 +6,21 @@
 //
 
 #include "demo1.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "common.h"
+
+int
+main(int argc, char *argv[]) {
+    int *p = malloc(sizeof(int));
+    assert(p != Null);
+    printf("(%d) memory address of p: %08x\n",getpid(),(unsigned) p);
+    *p = 0;
+    while(1) {
+        Spin(1);
+        *p = *p + 1;
+        printf("(%d) p: %d\n", getid(), *p);
+    }
+    return 0;
+}
