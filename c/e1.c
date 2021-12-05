9fd2f0ee6134464cc9201fd40bee48f8b8351858
han guo <guohan741@163.com>
2021-12-05 20:31:00 +0800

    commit demo

---
+++ c/e1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <string.h>
+#include <sys/wait.h>
+int main(int argc, char *argv[]) {
+//打印进程id
+	//printf("hello world (pid:%d)\n",(int)getpid());
+	int x = 1;
+	int rc = fork();
+
+	if(rc < 0) {
+		fprintf(stderr,"fork failed\n");
+		exit(1);
+	}else if(rc == 0) {
+
+		printf("子进程(pid:%d) x的值为%d \n",(int)getpid(),x);
+	}else {
+		//int wc = wait(NULL);
+		x = 100;
+		printf("(父进程pid:%d) x的值为%d \n",(int)getpid(),x);
+	}
+	return 0;
+
+}
