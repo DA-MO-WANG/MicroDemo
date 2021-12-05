8cd77a1331c3e09cf54fb3c00ec53d09f231b529
han guo <guohan741@163.com>
2021-12-05 19:56:00 +0800

    commit demo

--- c/p4.c
+++ c/p4.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[]) {
 		exit(1);
 	}else if(rc == 0) {
 		close(STDOUT_FILENO);
-		open("./p4.output",O_CREATE|O_WRONLY|O_TRUNC,S_IRWXU);
+		open("./p4.output",O_CREAT|O_WRONLY|O_TRUNC,S_IRWXU);
 
 		char *myargs[3];
 		myargs[0] = strdup("wc");
