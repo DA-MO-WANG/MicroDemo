4d3f7819d8bb7a5790118598ab8a3fe3c2081785
han guo <guohan741@163.com>
2022-01-21 17:57:01 +0800

    commit demo

--- java/springclouddemo/feigndemo/src/main/java/com/example/feigndemo/service/CityClient.java
+++ java/springclouddemo/feigndemo/src/main/java/com/example/feigndemo/service/CityClient.java
@@ -3,7 +3,7 @@ package com.example.feigndemo.service;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(value = "city")
+@FeignClient(value = "city",fallback = CityClientImpl.class)
 public interface CityClient {
     @GetMapping("cityuTest")
     String select();
--- java/springclouddemo/feigndemo/src/main/java/com/example/feigndemo/service/CityClientImpl.java
+++ java/springclouddemo/feigndemo/src/main/java/com/example/feigndemo/service/CityClientImpl.java
@@ -1,2 +1,11 @@
-package com.example.feigndemo.service;public class CityClientImpl {
+package com.example.feigndemo.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CityClientImpl implements CityClient{
+    @Override
+    public String select() {
+        return "调用失败";
+    }
 }
