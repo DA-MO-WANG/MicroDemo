fc5bf6b6e157434f5dd52a939b0efe6088025fe8
han guo <guohan741@163.com>
2022-01-30 16:18:01 +0800

    commit demo

--- shell/line.sh
+++ shell/line.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+# 井号注释的作用
+# ls 指令的结果，重定向到 execfile这个文件中
 #ls *.sh > execfile
+#while循环来读取每一行，至于哪里的每一行看后面
 while read line
+#循环中要做的事
 do 
 	echo $line
+#把execfile文件中的东西重定向到done这个命令中
 done <execfile.txt
