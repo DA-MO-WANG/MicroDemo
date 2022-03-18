71a2a223f9bdea81178a694d9bddc6e958f7cb55
DA-MO-WANG <guohan741@163.com>
2022-03-18 17:13:00 +0800

    commit demo

--- xml_transfer_javaentity/src/com/company/javax_xml_bind/JavaEntityBody.java
+++ xml_transfer_javaentity/src/com/company/javax_xml_bind/JavaEntityBody.java
@@ -8,7 +8,7 @@ import java.util.List;
 @XmlAccessorOrder(XmlAccessOrder.ALPHABETICAL)
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "RequestBody",propOrder = {
-        "name","apIdType","certNo","embName"
+        "name","apIdType","certNo","embName","addressEntityLists"
 })
 public class JavaEntityBody {
     @XmlElement(name = "name")
