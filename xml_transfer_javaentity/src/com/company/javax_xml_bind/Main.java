b3ed6784b629b65aa6b52b95a2f864e0b2b7483b
DA-MO-WANG <guohan741@163.com>
2022-03-22 10:58:00 +0800

    commit demo

--- xml_transfer_javaentity/src/com/company/javax_xml_bind/Main.java
+++ xml_transfer_javaentity/src/com/company/javax_xml_bind/Main.java
@@ -1,8 +1,14 @@
 package com.company.javax_xml_bind;
 
+import java.util.Calendar;
+
 public class Main {
     public static void main(String[] args) {
-        JavaEntityBody body = new JavaEntityBody();
+        //JavaEntityBody body = new JavaEntityBody();
+        Calendar calendar = Calendar.getInstance();
+        String calendarType = calendar.getCalendarType();
+        System.out.println(calendarType);
+
 
     }
 }
