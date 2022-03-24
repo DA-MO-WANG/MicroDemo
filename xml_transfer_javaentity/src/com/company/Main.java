8627e64ec7024bd1a826dcd597cbc2766d6290d9
DA-MO-WANG <guohan741@163.com>
2022-03-24 17:41:00 +0800

    commit demo

--- xml_transfer_javaentity/src/com/company/Main.java
+++ xml_transfer_javaentity/src/com/company/Main.java
@@ -14,57 +14,62 @@ import org.jdom2.JDOMException;
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
 import java.text.DateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.LinkedList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class Main {
     private static Logger log = LoggerFactory.getLogger(Main.class);
+//    public static void main(String[] args) {
+//	// write your code here
+//        RequestInfo requestObj = new RequestInfo();
+//        JavaEntityBody body = new JavaEntityBody();
+//        List<AddressEntity>  addressList = new ArrayList<>();
+//        for (int i = 0; i < 3; i++) {
+//            AddressEntity addressEntity = new AddressEntity();
+//            addressEntity.setCity("Beijing"+i);
+//            addressEntity.setDistrict("fangshan"+i);
+//            addressEntity.setState("hebeisheng"+i);
+//            addressList.add(addressEntity);
+//        }
+//
+//        body.setAddressEntityList(addressList);
+//
+//        body.setApIdType("222222");
+//        body.setCertNo("11111111111111111");
+//        body.setEmbName("hejjjjjjjjjjjjjjjj");
+//        body.setName("jjj");
+//        JavaEntityHeader requestHeader = new JavaEntityHeader();
+//        requestHeader.setChn1("10000000000");
+//        requestHeader.setChnlDate("20220902");
+//        requestHeader.setChnlTime("1010101001");
+//        requestHeader.setOpenId("2333333333333");
+//        requestObj.setHeader(requestHeader);
+//        requestObj.setBody(body);
+//        LinkedList list = new LinkedList();
+//        list.add(requestObj);
+//        //list.add(body);
+//        String res = "null";
+//        JSONObject json = null;
+//        try {
+//            res = JaxpUtil.toXML(requestObj);
+//            res = JSONObject.toJSON(list).toString();
+//            //json = XmlUtils.xml2Json(res);
+//        } catch (JAXBException e) {
+//            e.printStackTrace();
+//            log.error("生成报文失败！");
+//
+//        } finally {
+//            System.out.println(res);
+//        }
+//
+//
+//
+//    }
+
     public static void main(String[] args) {
-	// write your code here
-        RequestInfo requestObj = new RequestInfo();
-        JavaEntityBody body = new JavaEntityBody();
-        List<AddressEntity>  addressList = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            AddressEntity addressEntity = new AddressEntity();
-            addressEntity.setCity("Beijing"+i);
-            addressEntity.setDistrict("fangshan"+i);
-            addressEntity.setState("hebeisheng"+i);
-            addressList.add(addressEntity);
-        }
-
-        body.setAddressEntityList(addressList);
-
-        body.setApIdType("222222");
-        body.setCertNo("11111111111111111");
-        body.setEmbName("hejjjjjjjjjjjjjjjj");
-        body.setName("jjj");
-        JavaEntityHeader requestHeader = new JavaEntityHeader();
-        requestHeader.setChn1("10000000000");
-        requestHeader.setChnlDate("20220902");
-        requestHeader.setChnlTime("1010101001");
-        requestHeader.setOpenId("2333333333333");
-        requestObj.setHeader(requestHeader);
-        requestObj.setBody(body);
-        LinkedList list = new LinkedList();
-        list.add(requestObj);
-        //list.add(body);
-        String res = "null";
-        JSONObject json = null;
-        try {
-            res = JaxpUtil.toXML(requestObj);
-            res = JSONObject.toJSON(list).toString();
-            //json = XmlUtils.xml2Json(res);
-        } catch (JAXBException e) {
-            e.printStackTrace();
-            log.error("生成报文失败！");
-
-        } finally {
-            System.out.println(res);
-        }
-
-
+        Date date = new Date();
+        String format = new SimpleDateFormat("hh:mm:ss").format(date);
+        System.out.println(format);
 
     }
 }
