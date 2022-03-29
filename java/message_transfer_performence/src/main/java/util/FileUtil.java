b1eda8a63e58762c4cabd936f00ddefff021a8ce
DA-MO-WANG <guohan741@163.com>
2022-03-29 15:28:00 +0800

    commit demo

--- java/message_transfer_performence/src/main/java/util/FileUtil.java
+++ java/message_transfer_performence/src/main/java/util/FileUtil.java
@@ -1,5 +1,22 @@
 package util;
 
+import java.io.*;
+
 public class FileUtil {
-    public static void readToBuffer(StringBuffer buffer,String )
+    public static void readToBuffer(StringBuffer buffer,String filePath) {
+        String res = "";
+        try {
+            InputStream is = new FileInputStream(filePath);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            res = reader.readLine();
+            while (res != null) {
+               buffer.append(res);
+               buffer.append("")
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
