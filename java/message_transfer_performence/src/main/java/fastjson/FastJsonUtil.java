94161b0bdd58ef45f50cc0e1f75f2edd570a8bfb
DA-MO-WANG <guohan741@163.com>
2022-03-29 15:20:00 +0800

    commit demo

--- java/message_transfer_performence/src/main/java/fastjson/FastJsonUtil.java
+++ java/message_transfer_performence/src/main/java/fastjson/FastJsonUtil.java
@@ -1,9 +1,12 @@
 package fastjson;
 
+import com.alibaba.fastjson.JSONObject;
 import fastjson.model.Root;
 
 public class FastJsonUtil {
-    public static Root parseByFastJson(String json) {
-
+    public static void parseByFastJson(String json) {
+        
+        Root root = (Root)JSONObject.parse(json);
+        System.out.println(root.toString());
     }
 }
--- java/message_transfer_performence/src/main/java/fastjson/model/Root.java
+++ java/message_transfer_performence/src/main/java/fastjson/model/Root.java
@@ -8,4 +8,25 @@ public class Root {
         this.service_header = service_header;
         this.service_body = service_body;
     }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "  \"service_header\": {\n" +
+                "    \"chnl_time\": "+service_header.getChnl_time()+",\n" +
+                "    \"open_id\": "+service_header.getOpen_id()+",\n" +
+                "    \"crypt\": "+service_header.getCrypt()+",\n" +
+                "    \"rsp_code\": "+service_header.getRsp_code()+",\n" +
+                "    \"chnl_date\": "+service_header.getChnl_date()+",\n" +
+                "    \"rsp_msg\": "+service_header.getRsp_msg()+",\n" +
+                "    \"chnl\": "+service_header.getChnl()+",\n" +
+                "    \"chnl_flow\": "+service_header.getChnl_flow()+
+                "  },\n" +
+                "  \"service_body\": {\n" +
+                "    \"activity_no\": "+service_body.getActivity_no()+",\n" +
+                "    \"activity_name\": "+service_body.getActivity_name()+",\n" +
+                "    \"annual_fee\": "+service_body.getAnnual_fee() +
+                "  }\n" +
+                "}";
+    }
 }
