cc457f1239fb6df29439d966d6e3048c1c9a2282
DA-MO-WANG <guohan741@163.com>
2022-03-29 16:07:00 +0800

    commit demo

--- java/message_transfer_performence/src/main/java/fastjson/FastJsonUtil.java
+++ java/message_transfer_performence/src/main/java/fastjson/FastJsonUtil.java
@@ -9,16 +9,18 @@ import util.FileUtil;
 
 public class FastJsonUtil {
     public static void parseByFastJson() {
-        String json =  FileUtil.readToBuffer("/Users/richheart/IdeaProjects/microdemo/java/message_transfer_performence/src/main/resources/static/hrbcn.json");
-        JSONObject jsonMap = (JSONObject)JSON.parse(json);
-        String headstr = JSON.toJSONString(jsonMap.get("service_header"));
-        String bodystr = JSON.toJSONString(jsonMap.get("service_body"));
-        Header service_header =  JSON.parseObject(headstr, Header.class);
-        Body service_body =  JSON.parseObject(bodystr, Body.class);
-        Root root = new Root();
-        root.setService_body(service_body);
-        root.setService_header(service_header);
+//        String json =  FileUtil.readToBuffer("/Users/richheart/IdeaProjects/microdemo/java/message_transfer_performence/src/main/resources/static/hrbcn.json");
+//        JSONObject jsonMap = (JSONObject)JSON.parse(json);
+//        String headstr = JSON.toJSONString(jsonMap.get("service_header"));
+//        String bodystr = JSON.toJSONString(jsonMap.get("service_body"));
+//        Header service_header =  JSON.parseObject(headstr, Header.class);
+//        Body service_body =  JSON.parseObject(bodystr, Body.class);
+//        Root root = new Root();
+//        root.setService_body(service_body);
+//        root.setService_header(service_header);
 
+        String json =  FileUtil.readToBuffer("/Users/richheart/IdeaProjects/microdemo/java/message_transfer_performence/src/main/resources/static/hrbcn.json");
+        Root root = (Root)JSON.parse(json);
         System.out.println(root.toString());
     }
 }
