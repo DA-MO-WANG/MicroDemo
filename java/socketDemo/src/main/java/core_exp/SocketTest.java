09c9feedec55e97b0e6e3032bdc84307d4042bc1
DA-MO-WANG <guohan741@163.com>
2022-03-30 23:29:00 +0800

    commit demo

--- java/socketDemo/src/main/java/core_exp/SocketTest.java
+++ java/socketDemo/src/main/java/core_exp/SocketTest.java
@@ -5,16 +5,13 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class SocketTest {
-    public static void main(String[] args) {
-        try {
-            Socket s = new Socket("time-a.nist.gov",13);
+    public static void main(String[] args) throws IOException {
+        try (Socket s = new Socket("time-a.nist.gov",13);){
             Scanner in = new Scanner(s.getInputStream(),"utf-8");
             while (in.hasNextLine()) {
                 String line = in.nextLine();
                 System.out.println(line);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
