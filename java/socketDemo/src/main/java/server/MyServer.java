62872eeeb4a3a0b67b6fd9317d7ece59205460b3
DA-MO-WANG <guohan741@163.com>
2022-03-30 21:59:00 +0800

    commit demo

--- java/socketDemo/src/main/java/server/MyServer.java
+++ java/socketDemo/src/main/java/server/MyServer.java
@@ -1,2 +1,10 @@
-package server;public class MyServer {
+package server;
+
+import java.net.Socket;
+
+public class MyServer {
+    public static void main(String[] args) {
+        
+        Socket
+    }
 }
