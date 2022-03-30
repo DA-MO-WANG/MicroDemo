865e9ba261522e253f4d50cd43075a179b20d9c6
DA-MO-WANG <guohan741@163.com>
2022-03-30 23:36:01 +0800

    commit demo

--- java/socketDemo/src/main/java/core_exp/SocketTest.java
+++ java/socketDemo/src/main/java/core_exp/SocketTest.java
@@ -2,23 +2,31 @@ package core_exp;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class SocketTest {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         //try的用法-可以不与catch绑定，可以抛出去
         //try 加一个小括号
 
         //打开我们这边的socket，就是和他通信
-        try (Socket s = new Socket("time-a.nist.gov",13);){
+        try {
             //所有的别人的响应信息都会放到socket的输入流
             //解析拿到别人给我们的信息
+            Socket s = new Socket("time-a.nist.gov",13);
             s.setSoTimeout(2);
             Scanner in = new Scanner(s.getInputStream(),"utf-8");
             while (in.hasNextLine()) {
                 String line = in.nextLine();
                 System.out.println(line);
             }
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            System.out.println(1111);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println(22222);
         }
     }
 }
