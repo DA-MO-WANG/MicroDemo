3935b305bde819dd97c07a20c5b664bc9341be1c
DA-MO-WANG <guohan741@163.com>
2022-03-30 23:35:00 +0800

    commit demo

--- java/socketDemo/src/main/java/core_exp/SocketTest.java
+++ java/socketDemo/src/main/java/core_exp/SocketTest.java
@@ -6,7 +6,14 @@ import java.util.Scanner;
 
 public class SocketTest {
     public static void main(String[] args) throws IOException {
+        //try的用法-可以不与catch绑定，可以抛出去
+        //try 加一个小括号
+
+        //打开我们这边的socket，就是和他通信
         try (Socket s = new Socket("time-a.nist.gov",13);){
+            //所有的别人的响应信息都会放到socket的输入流
+            //解析拿到别人给我们的信息
+            s.setSoTimeout(2);
             Scanner in = new Scanner(s.getInputStream(),"utf-8");
             while (in.hasNextLine()) {
                 String line = in.nextLine();
