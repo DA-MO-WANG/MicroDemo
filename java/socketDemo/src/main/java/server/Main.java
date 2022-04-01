202a113ad8b0ea2939d930e1d64ba0add24bb0b1
DA-MO-WANG <guohan741@163.com>
2022-04-01 15:33:00 +0800

    commit demo

--- java/socketDemo/src/main/java/server/Main.java
+++ java/socketDemo/src/main/java/server/Main.java
@@ -5,7 +5,7 @@ public class Main {
         int x = -9;
         int fourthBitFromRight = (x & (1 << 3)) >>>3 ;
         System.out.println(fourthBitFromRight);
-        System.out.println(-1>>3);
+        System.out.println(-1>>>3);
 
     }
 }
