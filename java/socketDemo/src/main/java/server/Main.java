64c7a1bc2807d750f6ee3547ceaca3bd1a7aae64
DA-MO-WANG <guohan741@163.com>
2022-04-01 14:42:00 +0800

    commit demo

--- java/socketDemo/src/main/java/server/Main.java
+++ java/socketDemo/src/main/java/server/Main.java
@@ -2,7 +2,8 @@ package server;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("\t郭晗");
-        System.out.println('\u2122');
+        int x = 8;
+        int fourthBitFromRight = (x & 0b1000) / 0b1000;
+        System.out.println(fourthBitFromRight);
     }
 }
