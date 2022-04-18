993d321b46a0ca5f441264938d79fe534311dd4c
DA-MO-WANG <guohan741@163.com>
2022-04-18 15:38:00 +0800

    commit demo

--- java/ioctest/src/main/java/test/FXNewsProvider.java
+++ java/ioctest/src/main/java/test/FXNewsProvider.java
@@ -1,2 +1,20 @@
-package test;public class FXNewsProvider {
+package test;
+
+
+import com.sun.tools.javac.util.ArrayUtils;
+
+public class FXNewsProvider {
+    private IFXNewsListener newsListener;
+    private IFXNewsPersister newPersistener;
+    public void getAndPersisNews() {
+        String[] newsIds = newsListener.getAvaliableNewsIds();
+        if (ArrayUtils.isEmpty(newsIds)) {
+            return;
+        }
+        for(String newsId : newsIds) {
+            FXNewsBean newsBean = newsListener.getNewsByPK(newsId);
+            newPersistener.persistNews(newsBean);
+            newsListener.postProcessIfNecessary(newsId);
+        }
+    }
 }
