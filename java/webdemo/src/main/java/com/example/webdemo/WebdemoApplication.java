c00e03c00d85065536c7edf681043b6f89b1d30c
DA-MO-WANG <guohan741@163.com>
2022-04-19 18:29:00 +0800

    commit demo

--- java/webdemo/src/main/java/com/example/webdemo/WebdemoApplication.java
+++ java/webdemo/src/main/java/com/example/webdemo/WebdemoApplication.java
@@ -1,8 +1,5 @@
 package com.example.webdemo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class WebdemoApplication {
 
