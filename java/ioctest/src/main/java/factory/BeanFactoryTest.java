af9529dc3215a1c6bb26ec1d828ca71ba12a6392
DA-MO-WANG <guohan741@163.com>
2022-04-20 14:37:00 +0800

    commit demo

--- java/ioctest/src/main/java/factory/BeanFactoryTest.java
+++ java/ioctest/src/main/java/factory/BeanFactoryTest.java
@@ -1,13 +1,21 @@
 package factory;
 
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.RootBeanDefinition;
+import test.FXNewsProvider;
 
 public class BeanFactoryTest {
 
     public static void main(String[] args) {
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
         BeanFactory container = (BeanFactory)bindViaCode(beanFactory);
+    }
+
+    public static BeanFactory bindViaCode(BeanDefinitionRegistry registry) {
+        AbstractBeanDefinition newsProvider = new RootBeanDefinition(FXNewsProvider.class,true)
     }
 
 }
