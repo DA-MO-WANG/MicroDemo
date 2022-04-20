3f338b97f3d6d1856da5ab412fdc5e8e9e7a7527
DA-MO-WANG <guohan741@163.com>
2022-04-20 14:43:00 +0800

    commit demo

--- java/ioctest/src/main/java/factory/BeanFactoryTest.java
+++ java/ioctest/src/main/java/factory/BeanFactoryTest.java
@@ -1,11 +1,13 @@
 package factory;
 
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.config.ConstructorArgumentValues;
 import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 import test.DowJonesNewsListener;
+import test.DowJonesNewsPersister;
 import test.FXNewsProvider;
 
 public class BeanFactoryTest {
@@ -18,6 +20,13 @@ public class BeanFactoryTest {
     public static BeanFactory bindViaCode(BeanDefinitionRegistry registry) {
         AbstractBeanDefinition newsProvider = new RootBeanDefinition(FXNewsProvider.class,1,true);
         AbstractBeanDefinition newsListener = new RootBeanDefinition(DowJonesNewsListener.class,1,true);
+        AbstractBeanDefinition newsPersister = new RootBeanDefinition(DowJonesNewsPersister.class,1,true);
+
+        registry.registerBeanDefinition("djNewsProvider",newsProvider);
+        registry.registerBeanDefinition("djListener",newsListener);
+        registry.registerBeanDefinition("djPersister",newsPersister);
+
+        ConstructorArgumentValues argValue = new ConstructorArgumentValues();
         
     }
 
