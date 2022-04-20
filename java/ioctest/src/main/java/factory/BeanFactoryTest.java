45ee0b3e7ab8dffe36786f3c027e72fc3a65a10f
DA-MO-WANG <guohan741@163.com>
2022-04-20 14:35:01 +0800

    commit demo

--- java/ioctest/src/main/java/factory/BeanFactoryTest.java
+++ java/ioctest/src/main/java/factory/BeanFactoryTest.java
@@ -1,11 +1,13 @@
 package factory;
 
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 
 public class BeanFactoryTest {
 
     public static void main(String[] args) {
-        DefaultListableBeanFactory beanFactory = new
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        BeanFactory container = (BeanFactory)bindViaCode(beanFactory);
     }
 
 }
