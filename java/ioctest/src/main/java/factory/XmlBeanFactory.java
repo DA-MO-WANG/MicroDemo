1d9463c120d53e4ad76d7c798108eae345f79fa3
DA-MO-WANG <guohan741@163.com>
2022-04-20 15:49:01 +0800

    commit demo

--- java/ioctest/src/main/java/factory/XmlBeanFactory.java
+++ java/ioctest/src/main/java/factory/XmlBeanFactory.java
@@ -1,2 +1,20 @@
-package factory;public class XmlBeanFactory {
+package factory;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import test.DowJonesNewsListener;
+
+public class XmlBeanFactory {
+    public static void main(String[] args) {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        BeanFactory container = (BeanFactory)bindViaXMLFile(beanFactory);
+        DowJonesNewsListener dowJonesNewsListener = (DowJonesNewsListener)container.getBean("djListener");
+        dowJonesNewsListener.postProcessIfNecessary("123");
+
+    }
+
+    public static BeanFactory bindViaXMLFile(BeanDefinitionRegistry registry) {
+        
+    }
 }
