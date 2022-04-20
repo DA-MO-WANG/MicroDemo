1ec2302a8b76034cf4765df4946140902c87e66c
DA-MO-WANG <guohan741@163.com>
2022-04-20 16:47:01 +0800

    commit demo

--- java/ioctest/src/main/java/factory/AnnotationBeanFactory.java
+++ java/ioctest/src/main/java/factory/AnnotationBeanFactory.java
@@ -6,7 +6,10 @@ import test.FXNewsProvider;
 
 public class AnnotationBeanFactory {
     public static void main(String[] args) {
-        //
+        //ApplicationContext容器，加载触发器配置
+        //xml  component-scan标注了要扫描的包范围
+        //@Compoment 标注了哪些类需要被注册到容器里
+        //@Autowired 标注了哪里需要被注入，以及注入什么对象
         ApplicationContext ctx = new ClassPathXmlApplicationContext("anonation-config.xml");
         FXNewsProvider newsProvider = (FXNewsProvider)ctx.getBean("FXNewsProvider");
         newsProvider.getAndPersisNews();
