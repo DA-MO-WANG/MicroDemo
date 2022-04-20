78fe9ac7717b5abab16b203de5a76d9135f8a84d
DA-MO-WANG <guohan741@163.com>
2022-04-20 16:41:00 +0800

    commit demo

--- java/ioctest/src/main/java/test/DowJonesNewsListener.java
+++ java/ioctest/src/main/java/test/DowJonesNewsListener.java
@@ -1,5 +1,8 @@
 package test;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class DowJonesNewsListener implements IFXNewsListener{
     @Override
     public String[] getAvaliableNewsIds() {
--- java/ioctest/src/main/java/test/DowJonesNewsPersister.java
+++ java/ioctest/src/main/java/test/DowJonesNewsPersister.java
@@ -1,6 +1,9 @@
 package test;
 
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class DowJonesNewsPersister implements IFXNewsPersister{
 
     @Override
