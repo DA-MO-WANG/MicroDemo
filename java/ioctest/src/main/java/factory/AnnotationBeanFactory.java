5cdf0074e0797d18151349e9e0171fbf12778d15
DA-MO-WANG <guohan741@163.com>
2022-04-20 16:31:01 +0800

    commit demo

--- java/ioctest/src/main/java/factory/AnnotationBeanFactory.java
+++ java/ioctest/src/main/java/factory/AnnotationBeanFactory.java
@@ -2,7 +2,10 @@ package factory;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import test.FXNewsProvider;
 
 public class AnnotationBeanFactory {
-    ApplicationContext ctx = new ClassPathXmlApplicationContext("")
+    ApplicationContext ctx = new ClassPathXmlApplicationContext("anonation-config.xml");
+    FXNewsProvider newsProvider = (FXNewsProvider)ctx.getBean("FXNewsProvider");
+    
 }
