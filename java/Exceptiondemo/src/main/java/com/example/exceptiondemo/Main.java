4c725033397effdc4a502905f7e928690bdd7a4d
DA-MO-WANG <guohan741@163.com>
2022-04-27 08:47:01 +0800

    commit demo

--- java/Exceptiondemo/src/main/java/com/example/exceptiondemo/Main.java
+++ java/Exceptiondemo/src/main/java/com/example/exceptiondemo/Main.java
@@ -1,2 +1,11 @@
-package com.example.exceptiondemo;public class Main {
+package com.example.exceptiondemo;
+
+public class Main {
+    //异常声明占位效果
+    public static void print() throws NullPointerException,ArithmeticException {
+        System.out.println(11111);
+    }
+    public static void main(String[] args) {
+        print();
+    }
 }
