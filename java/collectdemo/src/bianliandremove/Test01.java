bf6ef5c8eb4d628df6a8805baad74115b05e56cf
DA-MO-WANG <guohan741@163.com>
2022-05-05 14:12:00 +0800

    commit demo

---
+++ java/collectdemo/src/bianliandremove/Test01.java
@@ -0,0 +1,8 @@
+package bianliandremove;/**
+ * @Description TO DO
+ * @Classname Test01
+ * @Date 2022/5/5 14:11
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class Test01 {
+}
