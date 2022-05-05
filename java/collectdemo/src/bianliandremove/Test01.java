ed31a2232f2b6b8cd18821f458b052962fa96cb6
DA-MO-WANG <guohan741@163.com>
2022-05-05 14:15:00 +0800

    commit demo

--- java/collectdemo/src/bianliandremove/Test01.java
+++ java/collectdemo/src/bianliandremove/Test01.java
@@ -1,8 +1,30 @@
-package bianliandremove;/**
+package bianliandremove;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
  * @Description TO DO
  * @Classname Test01
  * @Date 2022/5/5 14:11
  * @Created by richheart
  * Copyright (c) All Rights Reserved, 2022.
- */public class Test01 {
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        List<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        Iterator<Integer> iterator = list.iterator();
+        while(iterator.hasNext()) {
+            Integer ele = iterator.next();
+            if(ele == 2) {
+                iterator.remove();
+            }
+        }
+        System.out.println(list.size());
+    }
 }
