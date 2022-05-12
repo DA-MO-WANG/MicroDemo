383af32bb5d05903a4d4d4788d0d5eb69fd51af2
DA-MO-WANG <guohan741@163.com>
2022-05-12 11:41:00 +0800

    commit demo

--- java/collectdemo/src/bianliandremove/Test01.java
+++ java/collectdemo/src/bianliandremove/Test01.java
@@ -13,18 +13,20 @@ import java.util.List;
  */
 public class Test01 {
     public static void main(String[] args) {
-        List<Integer> list = new LinkedList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-
-        Iterator<Integer> iterator = list.iterator();
-        while(iterator.hasNext()) {
-            Integer ele = iterator.next();
-            if(ele == 2) {
-                iterator.remove();
-            }
-        }
-        System.out.println(list.size());
+//        List<Integer> list = new LinkedList<>();
+//        list.add(1);
+//        list.add(2);
+//        list.add(3);
+//
+//        Iterator<Integer> iterator = list.iterator();
+//        while(iterator.hasNext()) {
+//            Integer ele = iterator.next();
+//            if(ele == 2) {
+//                iterator.remove();
+//            }
+//        }
+//        System.out.println(list.size());
+        Double d = 12.2;
+        System.out.println(d+"");
     }
 }
