ce213ebff4da899febbcfb8d61e846a333d61bd6
DA-MO-WANG <guohan741@163.com>
2022-05-13 18:38:01 +0800

    commit demo

---
+++ java/multithreaddemo/src/main/java/zuseyuhuifu/Pojo1.java
@@ -0,0 +1,8 @@
+package zuseyuhuifu;/**
+ * @Description TO DO
+ * @Classname Pojo1
+ * @Date 2022/5/13 18:37
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class Pojo1 {
+}
