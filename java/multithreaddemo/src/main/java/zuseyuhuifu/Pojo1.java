f40375581dadea81cab8215efdd878c8414c079b
DA-MO-WANG <guohan741@163.com>
2022-05-13 18:43:00 +0800

    commit demo

--- java/multithreaddemo/src/main/java/zuseyuhuifu/Pojo1.java
+++ java/multithreaddemo/src/main/java/zuseyuhuifu/Pojo1.java
@@ -1,8 +1,15 @@
-package zuseyuhuifu;/**
+package zuseyuhuifu;
+
+/**
  * @Description TO DO
  * @Classname Pojo1
  * @Date 2022/5/13 18:37
  * @Created by richheart
  * Copyright (c) All Rights Reserved, 2022.
- */public class Pojo1 {
+ */
+public class Pojo1 implements Runnable{
+    @Override
+    public void run() {
+
+    }
 }
--- java/multithreaddemo/src/main/java/zuseyuhuifu/Test01.java
+++ java/multithreaddemo/src/main/java/zuseyuhuifu/Test01.java
@@ -1,8 +1,27 @@
-package zuseyuhuifu;/**
+package zuseyuhuifu;
+
+/**
  * @Description TO DO
  * @Classname test01
  * @Date 2022/5/13 18:36
  * @Created by richheart
  * Copyright (c) All Rights Reserved, 2022.
- */public class test01 {
+ */
+public class Test01 {
+    static int x = 10;
+    public static void main(String[] args) {
+
+        if (x == 20) {
+            System.out.println(1000000000);
+        }
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                x = 20;
+
+            }
+        });
+        thread1.start();
+
+    }
 }
