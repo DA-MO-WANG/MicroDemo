3ca2a3c4a822b8449ceb2085a541378712f4e1e8
DA-MO-WANG <guohan741@163.com>
2022-05-13 18:37:00 +0800

    commit demo

---
+++ java/multithreaddemo/src/main/java/zuseyuhuifu/test01.java
@@ -0,0 +1,8 @@
+package zuseyuhuifu;/**
+ * @Description TO DO
+ * @Classname test01
+ * @Date 2022/5/13 18:36
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class test01 {
+}
