ae8cc70f1feef149ec90afe333e1e6636a5b77c8
DA-MO-WANG <guohan741@163.com>
2022-06-09 14:06:00 +0800

    commit demo

---
+++ java/java_excel/src/main/java/com/example/java_excel/JavaExcelApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_excel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaExcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaExcelApplication.class, args);
+	}
+
+}
