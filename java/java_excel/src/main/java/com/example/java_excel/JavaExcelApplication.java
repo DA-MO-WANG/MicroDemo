48ca11641ef928d825886a8cdbef11fb73b0ce5b
DA-MO-WANG <guohan741@163.com>
2022-06-09 14:07:00 +0800

    commit demo

--- java/java_excel/src/main/java/com/example/java_excel/JavaExcelApplication.java
+++ java/java_excel/src/main/java/com/example/java_excel/JavaExcelApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaExcelApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaExcelApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JavaExcelApplication.class, args);
+    }
 
 }
--- java/java_excel/src/test/java/com/example/java_excel/JavaExcelApplicationTests.java
+++ java/java_excel/src/test/java/com/example/java_excel/JavaExcelApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class JavaExcelApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
