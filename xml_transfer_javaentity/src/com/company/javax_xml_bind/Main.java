0c8b9bd2074c3bc84cb4679e5adf4b3d744c4cf4
DA-MO-WANG <guohan741@163.com>
2022-09-06 14:42:00 +0800

    commit demo

--- xml_transfer_javaentity/src/com/company/javax_xml_bind/Main.java
+++ xml_transfer_javaentity/src/com/company/javax_xml_bind/Main.java
@@ -9,11 +9,8 @@ public class Main {
 //        Calendar calendar = Calendar.getInstance();
 //        String calendarType = calendar.getCalendarType();
 //        System.out.println(calendarType);
-        try {
-            System.out.println(test01());
-        }catch (RuntimeException e) {
-            System.out.println("1111111");
-        }
+
+        System.out.println(test01());
     }
 
     private static String test01() {
@@ -21,6 +18,7 @@ public class Main {
             int a = 5 / 0;
         }catch (RuntimeException e) {
             throw new RuntimeException(e);
+
         }finally {
             return "2222222";
         }
