831fef61d59f052193ed802001392859ffdeac39
DA-MO-WANG <guohan741@163.com>
2022-09-10 18:44:00 +0800

    commit demo

--- java/myspringcloud/src/test/java/com/example/myspringcloud/MyspringcloudApplicationTests.java
+++ java/myspringcloud/src/test/java/com/example/myspringcloud/MyspringcloudApplicationTests.java
@@ -1,14 +1,34 @@
 package com.example.myspringcloud;
 
+import com.example.myspringcloud.controller.App;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class MyspringcloudApplicationTests {
-
-    @Test
-    void contextLoads() {
+    private MockMvc mvc;
+    @Before
+    public void setUp() {
+        mvc = MockMvcBuilders.standaloneSetup(new App()).build();
     }
-    
+    @Test
+    void contextLoads() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("hello")));
+    }
+
 
 }
