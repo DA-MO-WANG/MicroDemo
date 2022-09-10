34bc1b52860d6c49ff1412a3b0824ba80ebff6eb
DA-MO-WANG <guohan741@163.com>
2022-09-10 20:33:00 +0800

    commit demo

---
+++ java/myspringcloud/microservice1/src/main/java/com/example/microservice1/controller/HelloController.java
@@ -0,0 +1,8 @@
+package com.example.microservice1.controller;/**
+ * @Description TO DO
+ * @Classname HelloController
+ * @Date 2022/9/10 20:32
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class HelloController {
+}
