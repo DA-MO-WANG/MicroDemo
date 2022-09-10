c3cc940d748dc2b4f7ff85644f8fef7652c1d6af
DA-MO-WANG <guohan741@163.com>
2022-09-10 18:24:00 +0800

    commit demo

---
+++ java/myspringcloud/src/main/java/com/example/myspringcloud/controller/App.java
@@ -0,0 +1,8 @@
+package com.example.myspringcloud.controller;/**
+ * @Description TO DO
+ * @Classname App
+ * @Date 2022/9/10 18:23
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class App {
+}
