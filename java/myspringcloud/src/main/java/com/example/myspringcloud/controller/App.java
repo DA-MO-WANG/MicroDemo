831a1f0c19953b4af955ee9076a38e254694365b
DA-MO-WANG <guohan741@163.com>
2022-09-10 18:26:01 +0800

    commit demo

--- java/myspringcloud/src/main/java/com/example/myspringcloud/controller/App.java
+++ java/myspringcloud/src/main/java/com/example/myspringcloud/controller/App.java
@@ -1,8 +1,19 @@
-package com.example.myspringcloud.controller;/**
+package com.example.myspringcloud.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
  * @Description TO DO
  * @Classname App
  * @Date 2022/9/10 18:23
  * @Created by richheart
  * Copyright (c) All Rights Reserved, 2022.
- */public class App {
+ */
+@RestController
+public class App {
+    @RequestMapping("/")
+    String home() {
+        return "hello";
+    }
 }
