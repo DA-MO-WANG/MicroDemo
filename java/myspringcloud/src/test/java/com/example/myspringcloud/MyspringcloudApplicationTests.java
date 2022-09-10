5eea54ac456fdd824f30ae009e7808a4ef1366dd
DA-MO-WANG <guohan741@163.com>
2022-09-10 18:53:00 +0800

    commit demo

--- java/myspringcloud/src/test/java/com/example/myspringcloud/MyspringcloudApplicationTests.java
+++ java/myspringcloud/src/test/java/com/example/myspringcloud/MyspringcloudApplicationTests.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -20,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
 class MyspringcloudApplicationTests {
     private MockMvc mvc;
     @Before
