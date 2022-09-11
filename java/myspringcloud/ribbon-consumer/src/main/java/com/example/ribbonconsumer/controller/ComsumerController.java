f11ba217969c87db1b5bee048c10a85e8a5a5741
DA-MO-WANG <guohan741@163.com>
2022-09-11 12:04:01 +0800

    commit demo

--- java/myspringcloud/ribbon-consumer/src/main/java/com/example/ribbonconsumer/controller/ComsumerController.java
+++ java/myspringcloud/ribbon-consumer/src/main/java/com/example/ribbonconsumer/controller/ComsumerController.java
@@ -1,8 +1,24 @@
-package com.example.ribbonconsumer.controller;/**
+package com.example.ribbonconsumer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
  * @Description TO DO
  * @Classname ComsumerController
  * @Date 2022/9/11 11:57
  * @Created by richheart
  * Copyright (c) All Rights Reserved, 2022.
- */public class ComsumerController {
+ */
+@RestController
+public class ComsumerController {
+    @Autowired
+    RestTemplate restTemplate;
+    @RequestMapping(value = "/ribbon-consumer",method = RequestMethod.GET)
+    public String helloConsumer() {
+        return restTemplate.getForEntity("hello://HELLO-SERVICE/hello",String.class).getBody();
+    }
 }
