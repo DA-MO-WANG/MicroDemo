c05c671febfcefee11e3644a817c55fd52dd8c97
DA-MO-WANG <guohan741@163.com>
2022-09-11 19:04:00 +0800

    commit demo

---
+++ java/myspringcloud/ribbon-consumer/src/main/java/com/example/ribbonconsumer/service/HelloService.java
@@ -0,0 +1,8 @@
+package com.example.ribbonconsumer.service;/**
+ * @Description TO DO
+ * @Classname HelloService
+ * @Date 2022/9/11 19:03
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class HelloService {
+}
