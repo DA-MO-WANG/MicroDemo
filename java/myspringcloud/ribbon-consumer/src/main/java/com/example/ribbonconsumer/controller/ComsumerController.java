75a1d462f293e60ce9c9ad18ab05053c5ff5c0e3
DA-MO-WANG <guohan741@163.com>
2022-09-11 11:58:00 +0800

    commit demo

---
+++ java/myspringcloud/ribbon-consumer/src/main/java/com/example/ribbonconsumer/controller/ComsumerController.java
@@ -0,0 +1,8 @@
+package com.example.ribbonconsumer.controller;/**
+ * @Description TO DO
+ * @Classname ComsumerController
+ * @Date 2022/9/11 11:57
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class ComsumerController {
+}
