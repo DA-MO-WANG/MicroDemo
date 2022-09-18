ed756d805cb159d85528470b717bcc4a24b2671e
DA-MO-WANG <zard20201116@gmail.com>
2022-09-18 21:28:00 +0800

    commit demo

---
+++ rust/world_hello/target/rls/debug/deps/world_hello-03a636ca7621899e.d
@@ -0,0 +1,5 @@
+/Users/richheart/IdeaProjects/microdemo/rust/world_hello/target/rls/debug/deps/world_hello-03a636ca7621899e.rmeta: src/main.rs
+
+/Users/richheart/IdeaProjects/microdemo/rust/world_hello/target/rls/debug/deps/world_hello-03a636ca7621899e.d: src/main.rs
+
+src/main.rs:
---
+++ rust/world_hello/target/rls/debug/deps/world_hello-5fc4e79150c510cd.d
@@ -0,0 +1,5 @@
+/Users/richheart/IdeaProjects/microdemo/rust/world_hello/target/rls/debug/deps/world_hello-5fc4e79150c510cd.rmeta: src/main.rs
+
+/Users/richheart/IdeaProjects/microdemo/rust/world_hello/target/rls/debug/deps/world_hello-5fc4e79150c510cd.d: src/main.rs
+
+src/main.rs:
