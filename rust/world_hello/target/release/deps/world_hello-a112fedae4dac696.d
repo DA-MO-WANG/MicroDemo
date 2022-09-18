797c4e80acac1eb250f7467f089fe89a0243047f
DA-MO-WANG <zard20201116@gmail.com>
2022-09-18 16:41:00 +0800

    commit demo

---
+++ rust/world_hello/target/release/deps/world_hello-a112fedae4dac696.d
@@ -0,0 +1,5 @@
+/Users/richheart/IdeaProjects/microdemo/rust/world_hello/target/release/deps/world_hello-a112fedae4dac696: src/main.rs
+
+/Users/richheart/IdeaProjects/microdemo/rust/world_hello/target/release/deps/world_hello-a112fedae4dac696.d: src/main.rs
+
+src/main.rs:
