eb7465c40d2eec516590577fc2eeb2045d90e36a
DA-MO-WANG <zard20201116@gmail.com>
2022-09-21 17:34:00 +0800

    commit demo

--- rust/world_hello/src/main.rs
+++ rust/world_hello/src/main.rs
@@ -1,5 +1,11 @@
+// fn main() {
+//   let mut name = 3;
+//   name = 4;
+//   println!("name参数值为{}", name);
+// }
 fn main() {
-  let mut name = 3;
-  name = 4;
-  println!("name参数值为{}", name);
+  let (a, mut b): (bool, bool) = (true, false);
+  println!("a = {:?}, b = {:?}", a, b);
+  b = true;
+  assert_eq!(a, b);
 }
