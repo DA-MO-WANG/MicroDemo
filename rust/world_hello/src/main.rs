687a65eb886cf4995fc45b0d05746e2f7861698e
DA-MO-WANG <zard20201116@gmail.com>
2022-10-20 16:11:01 +0800

    commit demo

--- rust/world_hello/src/main.rs
+++ rust/world_hello/src/main.rs
@@ -1,8 +1,14 @@
 fn main() {
-    let x = "out";
-    {
-        let x = "in";
-        println!("{}", x);
-    }
-    println!("{}", x);
+    // let x = "out";
+    // {
+    //     let x = "in";
+    //     println!("{}", x);
+    // }
+    // println!("{}", x);
+    let res = fair_dice_roll();
+    println!(res);
+}
+
+fn fair_dice_roll() -> i32 {
+    return 4;
 }
