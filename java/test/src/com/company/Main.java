297c833e6a05827b74f06ecf81b372c82993625a
DA-MO-WANG <zard20201116@gmail.com>
2022-11-14 18:05:00 +0800

    commit demo

--- java/test/src/com/company/Main.java
+++ java/test/src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        double x = 1.007;
+        double x = 0.0007;
         System.out.println(x);
     }
 }
