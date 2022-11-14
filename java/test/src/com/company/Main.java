ad6d6b077a5fe1966c9772a7da5d166ee0d21d72
DA-MO-WANG <zard20201116@gmail.com>
2022-11-14 18:11:00 +0800

    commit demo

--- java/test/src/com/company/Main.java
+++ java/test/src/com/company/Main.java
@@ -1,10 +1,14 @@
 package com.company;
 
+import java.text.DecimalFormat;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         double x = 0.0000001;
-        System.out.println(x);
+        DecimalFormat d = new DecimalFormat("#,##0.00000000");
+        String format = d.format(x);
+        System.out.println(format);
     }
 }
