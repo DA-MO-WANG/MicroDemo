46a0daab74f2189c8cff7f3108aae02d4352df8e
DA-MO-WANG <zard20201116@gmail.com>
2022-11-18 16:52:00 +0800

    commit demo

---
+++ java/transactionTest1/src/main/java/com/example/transactiontest1/TransactionTest1Application.java
@@ -0,0 +1,13 @@
+package com.example.transactiontest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionTest1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionTest1Application.class, args);
+    }
+
+}
---
+++ java/transactionTest1/src/test/java/com/example/transactiontest1/TransactionTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transactiontest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionTest1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
