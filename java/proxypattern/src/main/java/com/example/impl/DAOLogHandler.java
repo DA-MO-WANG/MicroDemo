1d0f6fd27e18de447eb4a548c36b208e31ef48cc
DA-MO-WANG <zard20201116@gmail.com>
2023-01-30 14:43:00 +0800

    commit demo

---
+++ java/proxypattern/src/main/java/com/example/impl/DAOLogHandler.java
@@ -0,0 +1,8 @@
+package com.example.impl;/**
+ * @Description TO DO
+ * @Classname DAOLogHandler
+ * @Date 2023/1/30 14:42
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class DAOLogHandler {
+}
