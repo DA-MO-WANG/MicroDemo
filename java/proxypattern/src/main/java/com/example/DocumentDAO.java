8cf1eb6d51fb68179a33f7d024cf3e466354a194
DA-MO-WANG <zard20201116@gmail.com>
2023-01-30 14:31:00 +0800

    commit demo

---
+++ java/proxypattern/src/main/java/com/example/DocumentDAO.java
@@ -0,0 +1,8 @@
+package com.example;/**
+ * @Description TO DO
+ * @Classname DocumentDAO
+ * @Date 2023/1/30 14:30
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public interface DocumentDAO {
+}
