71fbc49b87c737573c5026e32c2054af18774ade
DA-MO-WANG <zard20201116@gmail.com>
2023-02-08 13:27:00 +0800

    commit demo

---
+++ java/designPattern/src/prototypePattern/Gaibang.java
@@ -0,0 +1,8 @@
+package prototypePattern;/**
+ * @Description TO DO
+ * @Classname Gaibang
+ * @Date 2023/2/8 13:26
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class Gaibang {
+}
