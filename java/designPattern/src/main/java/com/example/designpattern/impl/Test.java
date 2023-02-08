36a96cdabd4618f4b8ce4401da4a49d34c244041
DA-MO-WANG <zard20201116@gmail.com>
2023-02-08 11:16:00 +0800

    commit demo

---
+++ java/designPattern/src/main/java/com/example/designpattern/impl/Test.java
@@ -0,0 +1,8 @@
+package com.example.designpattern.impl;/**
+ * @Description TO DO
+ * @Classname Test
+ * @Date 2023/2/8 11:15
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class Test {
+}
