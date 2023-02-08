b3d33d8043154145f39a178be4930b940d0152b2
DA-MO-WANG <zard20201116@gmail.com>
2023-02-08 13:33:01 +0800

    commit demo

---
+++ java/designPattern/src/prototypePattern/Test.java
@@ -0,0 +1,8 @@
+package prototypePattern;/**
+ * @Description TO DO
+ * @Classname Test
+ * @Date 2023/2/8 13:32
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class Test {
+}
