4bd5c461a2b23cb195cc2243fac884343217f6e7
DA-MO-WANG <zard20201116@gmail.com>
2023-02-08 14:03:01 +0800

    commit demo

---
+++ java/proxypattern/src/main/java/com/example/prototypepattern/Test.java
@@ -0,0 +1,8 @@
+package com.example.prototypepattern;/**
+ * @Description TO DO
+ * @Classname Test
+ * @Date 2023/2/8 14:02
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class Test {
+}
