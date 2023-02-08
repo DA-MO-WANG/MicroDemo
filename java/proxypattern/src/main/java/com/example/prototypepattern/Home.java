35f4b638a255daed100ffb4b0d506b685c87f2fe
DA-MO-WANG <zard20201116@gmail.com>
2023-02-08 13:56:00 +0800

    commit demo

---
+++ java/proxypattern/src/main/java/com/example/prototypepattern/Home.java
@@ -0,0 +1,8 @@
+package com.example.prototypepattern;/**
+ * @Description TO DO
+ * @Classname Home
+ * @Date 2023/2/8 13:55
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class Home {
+}
