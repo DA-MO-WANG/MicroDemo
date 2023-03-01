f02eb707c5b27153f9374e109649bbab3f017d63
DA-MO-WANG <zard20201116@gmail.com>
2023-03-01 09:05:00 +0800

    commit demo

---
+++ java/observePattern/src/main/java/pattern/ObservePatternTest.java
@@ -0,0 +1,8 @@
+package pattern;/**
+ * @Description TO DO
+ * @Classname ObservePatternTest
+ * @Date 2023/3/1 09:04
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class ObservePatternTest {
+}
