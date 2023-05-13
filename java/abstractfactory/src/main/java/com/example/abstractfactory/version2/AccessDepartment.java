f0c90f0a63ae068bba2c0a86c8cd9cc3897c1165
DA-MO-WANG <zard20201116@gmail.com>
2023-05-13 11:07:00 +0800

    commit demo

---
+++ java/abstractfactory/src/main/java/com/example/abstractfactory/version2/AccessDepartment.java
@@ -0,0 +1,8 @@
+package com.example.abstractfactory.version2;/**
+ * @Description TO DO
+ * @Classname AccessDepartment
+ * @Date 2023/5/13 11:06
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class AccessDepartment {
+}
