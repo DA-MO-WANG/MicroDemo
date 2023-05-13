db5834a488ca6f1c315c2d406843eb636150abe5
DA-MO-WANG <zard20201116@gmail.com>
2023-05-13 15:47:00 +0800

    commit demo

---
+++ java/abstractfactory/src/main/java/com/example/abstractfactory/utils/GetParameters.java
@@ -0,0 +1,8 @@
+package com.example.abstractfactory.utils;/**
+ * @Description TO DO
+ * @Classname GetParameters
+ * @Date 2023/5/13 15:46
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class GetParameters {
+}
