b2190206eda2706d577621a5310af08fad8d6609
DA-MO-WANG <zard20201116@gmail.com>
2023-05-13 10:57:00 +0800

    commit demo

---
+++ java/abstractfactory/src/main/java/com/example/abstractfactory/version2/AccessFactory.java
@@ -0,0 +1,8 @@
+package com.example.abstractfactory.version2;/**
+ * @Description TO DO
+ * @Classname AccessFactory
+ * @Date 2023/5/13 10:56
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class AccessFactory {
+}
