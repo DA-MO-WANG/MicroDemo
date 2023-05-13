1ff9b6a5b5e15b2a33c8bf4d5485480e37c53159
DA-MO-WANG <zard20201116@gmail.com>
2023-05-13 10:37:01 +0800

    commit demo

---
+++ java/abstractfactory/src/main/java/com/example/abstractfactory/entity/User.java
@@ -0,0 +1,8 @@
+package com.example.abstractfactory.entity;/**
+ * @Description TO DO
+ * @Classname User
+ * @Date 2023/5/13 10:36
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class User {
+}
