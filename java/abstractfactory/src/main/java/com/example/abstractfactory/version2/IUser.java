1013a60c401f035f9464183b6f5024bcec944e7f
DA-MO-WANG <zard20201116@gmail.com>
2023-05-13 10:47:00 +0800

    commit demo

---
+++ java/abstractfactory/src/main/java/com/example/abstractfactory/version2/IUser.java
@@ -0,0 +1,8 @@
+package com.example.abstractfactory.version2;/**
+ * @Description TO DO
+ * @Classname IUser
+ * @Date 2023/5/13 10:46
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public interface IUser {
+}
