52e0d308057d2ea1e31a30c5777e953681cef55d
DA-MO-WANG <zard20201116@gmail.com>
2023-05-13 15:48:00 +0800

    commit demo

--- java/abstractfactory/src/main/java/com/example/abstractfactory/utils/cdf.java
+++
@@ -1,8 +0,0 @@
-package com.example.abstractfactory.utils;/**
- * @Description TO DO
- * @Classname cdf
- * @Date 2023/5/13 15:46
- * @Created by richheart
- * Copyright (c) All Rights Reserved, 2022.
- */public class cdf {
-}
