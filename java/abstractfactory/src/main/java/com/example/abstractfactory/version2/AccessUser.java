a5607f7bb30cb62f37815113a3c592fe23778c03
DA-MO-WANG <zard20201116@gmail.com>
2023-05-13 10:52:01 +0800

    commit demo

---
+++ java/abstractfactory/src/main/java/com/example/abstractfactory/version2/AccessUser.java
@@ -0,0 +1,8 @@
+package com.example.abstractfactory.version2;/**
+ * @Description TO DO
+ * @Classname AccessUser
+ * @Date 2023/5/13 10:51
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class AccessUser {
+}
