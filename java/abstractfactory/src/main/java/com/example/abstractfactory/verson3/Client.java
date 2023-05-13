f19af68cd72943cfa269b63ba4358ea7a8f63a05
DA-MO-WANG <zard20201116@gmail.com>
2023-05-13 14:28:00 +0800

    commit demo

---
+++ java/abstractfactory/src/main/java/com/example/abstractfactory/verson3/Client.java
@@ -0,0 +1,8 @@
+package com.example.abstractfactory.verson3;/**
+ * @Description TO DO
+ * @Classname Client
+ * @Date 2023/5/13 14:27
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class Client {
+}
