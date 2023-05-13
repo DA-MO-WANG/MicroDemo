72ece2061657ecfb8b6106d4853e7b3217d4dc11
DA-MO-WANG <zard20201116@gmail.com>
2023-05-13 10:49:00 +0800

    commit demo

---
+++ java/abstractfactory/src/main/java/com/example/abstractfactory/version2/SqlserverUser.java
@@ -0,0 +1,8 @@
+package com.example.abstractfactory.version2;/**
+ * @Description TO DO
+ * @Classname SqlserverUser
+ * @Date 2023/5/13 10:48
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class SqlserverUser {
+}
