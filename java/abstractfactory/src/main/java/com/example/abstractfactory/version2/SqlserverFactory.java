fb750a61eaa1b7979b388b8ad6531ffbfbd53cf5
DA-MO-WANG <zard20201116@gmail.com>
2023-05-13 10:55:00 +0800

    commit demo

---
+++ java/abstractfactory/src/main/java/com/example/abstractfactory/version2/SqlserverFactory.java
@@ -0,0 +1,8 @@
+package com.example.abstractfactory.version2;/**
+ * @Description TO DO
+ * @Classname SqlserverFactory
+ * @Date 2023/5/13 10:54
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class SqlserverFactory {
+}
