7bc046a486835516f2c94d6749243bc0379a54ed
DA-MO-WANG <zard20201116@gmail.com>
2023-05-14 10:54:01 +0800

    commit demo

---
+++ java/statefactory/src/main/java/com/example/version1/WorkState.java
@@ -0,0 +1,8 @@
+package com.example.version1;/**
+ * @Description TO DO
+ * @Classname WorkState
+ * @Date 2023/5/14 10:53
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class WorkState {
+}
