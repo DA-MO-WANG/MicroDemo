c9ff0f49d0da93995f7ecbab4b378409c0432113
DA-MO-WANG <zard20201116@gmail.com>
2023-05-14 11:47:01 +0800

    commit demo

---
+++ java/statefactory/src/main/java/com/example/version3_2/RestState.java
@@ -0,0 +1,8 @@
+package com.example.version3_2;/**
+ * @Description TO DO
+ * @Classname RestState
+ * @Date 2023/5/14 11:46
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class RestState {
+}
