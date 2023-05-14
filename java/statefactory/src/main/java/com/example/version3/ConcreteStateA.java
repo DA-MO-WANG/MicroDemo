64a5da188f40852ff401178dd6e4aa17178200ae
DA-MO-WANG <zard20201116@gmail.com>
2023-05-14 11:20:00 +0800

    commit demo

---
+++ java/statefactory/src/main/java/com/example/version3/ConcreteStateA.java
@@ -0,0 +1,8 @@
+package com.example.version3;/**
+ * @Description TO DO
+ * @Classname ConcreteStateA
+ * @Date 2023/5/14 11:19
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class ConcreteStateA {
+}
