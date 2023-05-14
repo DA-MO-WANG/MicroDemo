865ac9c650ef4c0dd74cde6b3565fa00aa015f10
DA-MO-WANG <zard20201116@gmail.com>
2023-05-14 11:36:00 +0800

    commit demo

---
+++ java/statefactory/src/main/java/com/example/version3_2/State.java
@@ -0,0 +1,8 @@
+package com.example.version4;/**
+ * @Description TO DO
+ * @Classname State
+ * @Date 2023/5/14 11:35
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class State {
+}
