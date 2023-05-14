d7debd207958200b2a1d466736d125c953765afc
DA-MO-WANG <zard20201116@gmail.com>
2023-05-14 11:37:00 +0800

    commit demo

---
+++ java/statefactory/src/main/java/com/example/version3_2/Work.java
@@ -0,0 +1,8 @@
+package com.example.version3_2;/**
+ * @Description TO DO
+ * @Classname Work
+ * @Date 2023/5/14 11:36
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class Work {
+}
