799d403c7eda22ed6cdfcd062de3a96d56d4a33f
DA-MO-WANG <zard20201116@gmail.com>
2023-05-14 11:46:01 +0800

    commit demo

---
+++ java/statefactory/src/main/java/com/example/version3_2/EveningState.java
@@ -0,0 +1,8 @@
+package com.example.version3_2;/**
+ * @Description TO DO
+ * @Classname EveningState
+ * @Date 2023/5/14 11:45
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class EveningState {
+}
