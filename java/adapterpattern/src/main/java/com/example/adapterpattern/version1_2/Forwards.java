ebd1656f9d49ddba64324fb86b7111b7d7243a3c
DA-MO-WANG <zard20201116@gmail.com>
2023-05-15 14:22:01 +0800

    commit demo

---
+++ java/adapterpattern/src/main/java/com/example/adapterpattern/version1_2/Forwards.java
@@ -0,0 +1,8 @@
+package com.example.adapterpattern.version1_2;/**
+ * @Description TO DO
+ * @Classname Forwards
+ * @Date 2023/5/15 14:21
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class Forwards {
+}
