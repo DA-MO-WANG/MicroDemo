d81dd24c8faa6a93b2195013a740468f092df4bd
DA-MO-WANG <zard20201116@gmail.com>
2023-05-15 14:27:00 +0800

    commit demo

---
+++ java/adapterpattern/src/main/java/com/example/adapterpattern/version1_2/Center.java
@@ -0,0 +1,8 @@
+package com.example.adapterpattern.version1_2;/**
+ * @Description TO DO
+ * @Classname Center
+ * @Date 2023/5/15 14:26
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class Center {
+}
