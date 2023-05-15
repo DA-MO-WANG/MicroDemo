05857df3bb57586fc128b67a366c9cee7af78a32
DA-MO-WANG <zard20201116@gmail.com>
2023-05-15 14:11:00 +0800

    commit demo

---
+++ java/adapterpattern/src/main/java/com/example/adapterpattern/version1/Target.java
@@ -0,0 +1,8 @@
+package com.example.adapterpattern.version1;/**
+ * @Description TO DO
+ * @Classname Target
+ * @Date 2023/5/15 14:10
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class Target {
+}
