7f3457f51204e06be67e341f6e2c71ec73f9c848
DA-MO-WANG <zard20201116@gmail.com>
2023-05-15 14:37:00 +0800

    commit demo

---
+++ java/adapterpattern/src/main/java/com/example/adapterpattern/version2/Client.java
@@ -0,0 +1,8 @@
+package com.example.adapterpattern.version2;/**
+ * @Description TO DO
+ * @Classname Client
+ * @Date 2023/5/15 14:36
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class Client {
+}
