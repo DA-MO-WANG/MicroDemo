fb917ddf1883aeac4a42a3888e46fd223817b344
DA-MO-WANG <zard20201116@gmail.com>
2023-05-16 14:08:00 +0800

    commit demo

---
+++ java/threadlocalbugdemo/src/main/java/com/example/demo2/ThreadLocalDemo2.java
@@ -0,0 +1,8 @@
+package com.example.demo2;/**
+ * @Description TO DO
+ * @Classname ThreadLocalDemo2
+ * @Date 2023/5/16 14:07
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class ThreadLocalDemo2 {
+}
