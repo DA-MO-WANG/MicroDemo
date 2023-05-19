5d9558952feb96bf3a761536f384a78ecaf66c0e
DA-MO-WANG <zard20201116@gmail.com>
2023-05-19 14:22:00 +0800

    commit demo

---
+++ java/lambda/src/main/java/com/example/lambda/version1/Demo2.java
@@ -0,0 +1,8 @@
+package com.example.lambda.version1;/**
+ * @Description TO DO
+ * @Classname Demo2
+ * @Date 2023/5/19 14:21
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class Demo2 {
+}
