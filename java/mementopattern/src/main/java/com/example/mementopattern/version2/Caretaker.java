baaf3750ae2a3f83b216fd4074f5e83b6c1bef36
DA-MO-WANG <zard20201116@gmail.com>
2023-05-19 15:56:00 +0800

    commit demo

---
+++ java/mementopattern/src/main/java/com/example/mementopattern/version2/Caretaker.java
@@ -0,0 +1,8 @@
+package com.example.mementopattern.version2;/**
+ * @Description TO DO
+ * @Classname Caretaker
+ * @Date 2023/5/19 15:55
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class Caretaker {
+}
