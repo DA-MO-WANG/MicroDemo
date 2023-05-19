d1edbbdbba83e48a17f03f212d7a8b31f94e9804
DA-MO-WANG <zard20201116@gmail.com>
2023-05-19 15:37:00 +0800

    commit demo

---
+++ java/mementopattern/src/main/java/com/example/mementopattern/version1/GameRole.java
@@ -0,0 +1,8 @@
+package com.example.mementopattern.version1;/**
+ * @Description TO DO
+ * @Classname GameRole
+ * @Date 2023/5/19 15:36
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class GameRole {
+}
