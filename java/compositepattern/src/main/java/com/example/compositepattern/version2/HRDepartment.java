bd383af3bc73f38ce0c49b488c3b2f71a6bcb94f
DA-MO-WANG <zard20201116@gmail.com>
2023-05-20 14:05:01 +0800

    commit demo

---
+++ java/compositepattern/src/main/java/com/example/compositepattern/version2/HRDepartment.java
@@ -0,0 +1,8 @@
+package com.example.compositepattern.version2;/**
+ * @Description TO DO
+ * @Classname HRDepartment
+ * @Date 2023/5/20 14:04
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class HRDepartment {
+}
