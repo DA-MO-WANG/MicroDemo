f7590de95c1b9a347c9d6721f5eed432ef0d5c9f
DA-MO-WANG <zard20201116@gmail.com>
2023-05-20 13:46:00 +0800

    commit demo

---
+++ java/compositepattern/src/main/java/com/example/compositepattern/version1/Client.java
@@ -0,0 +1,8 @@
+package com.example.compositepattern.version1;/**
+ * @Description TO DO
+ * @Classname Client
+ * @Date 2023/5/20 13:45
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class Client {
+}
