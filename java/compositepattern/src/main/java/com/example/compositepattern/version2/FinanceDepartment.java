0b7c3f855ab236faab70f23d684aa7d122a2cf91
DA-MO-WANG <zard20201116@gmail.com>
2023-05-20 14:07:01 +0800

    commit demo

---
+++ java/compositepattern/src/main/java/com/example/compositepattern/version2/FinanceDepartment.java
@@ -0,0 +1,8 @@
+package com.example.compositepattern.version2;/**
+ * @Description TO DO
+ * @Classname FinanceDepartment
+ * @Date 2023/5/20 14:06
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class FinanceDepartment {
+}
