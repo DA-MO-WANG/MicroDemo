c72f72a12194dc87d43b049d75ef72809288331e
DA-MO-WANG <zard20201116@gmail.com>
2023-05-20 13:43:00 +0800

    commit demo

---
+++ java/compositepattern/src/main/java/com/example/compositepattern/version1/Composite.java
@@ -0,0 +1,8 @@
+package com.example.compositepattern.version1;/**
+ * @Description TO DO
+ * @Classname Composite
+ * @Date 2023/5/20 13:42
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class Composite {
+}
