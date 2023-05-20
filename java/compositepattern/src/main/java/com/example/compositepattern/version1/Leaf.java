fb94277aebe4ca73456abe8b967c0263b0ccde09
DA-MO-WANG <zard20201116@gmail.com>
2023-05-20 13:39:01 +0800

    commit demo

---
+++ java/compositepattern/src/main/java/com/example/compositepattern/version1/Leaf.java
@@ -0,0 +1,8 @@
+package com.example.compositepattern.version1;/**
+ * @Description TO DO
+ * @Classname Leaf
+ * @Date 2023/5/20 13:38
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class Leaf {
+}
