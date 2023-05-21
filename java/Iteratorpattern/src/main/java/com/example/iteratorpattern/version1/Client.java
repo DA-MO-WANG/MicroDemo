bd0a8361204175867cf08bf1d719086820822ef2
DA-MO-WANG <zard20201116@gmail.com>
2023-05-21 14:03:01 +0800

    commit demo

---
+++ java/Iteratorpattern/src/main/java/com/example/iteratorpattern/version1/Client.java
@@ -0,0 +1,8 @@
+package com.example.iteratorpattern.version1;/**
+ * @Description TO DO
+ * @Classname Client
+ * @Date 2023/5/21 14:02
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class Client {
+}
