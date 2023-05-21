e013cca8d9e84839efb647cb71533720a80871ba
DA-MO-WANG <zard20201116@gmail.com>
2023-05-21 13:49:01 +0800

    commit demo

---
+++ java/Iteratorpattern/src/main/java/com/example/iteratorpattern/version1/ConcreteIterator.java
@@ -0,0 +1,8 @@
+package com.example.iteratorpattern.version1;/**
+ * @Description TO DO
+ * @Classname ConcreteIterator
+ * @Date 2023/5/21 13:48
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class ConcreteIterator {
+}
