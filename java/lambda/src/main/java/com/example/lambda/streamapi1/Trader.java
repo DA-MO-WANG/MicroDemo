e8f1d80019460440316e9256fae1404fada673d6
DA-MO-WANG <zard20201116@gmail.com>
2023-05-23 16:43:01 +0800

    commit demo

---
+++ java/lambda/src/main/java/com/example/lambda/streamapi1/Trader.java
@@ -0,0 +1,8 @@
+package com.example.lambda.streamapi1;/**
+ * @Description TO DO
+ * @Classname Trader
+ * @Date 2023/5/23 16:42
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class Trader {
+}
