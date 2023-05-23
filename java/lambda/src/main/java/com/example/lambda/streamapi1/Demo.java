db7e7ef45c1206c74bcfb9920c4e15a256faf90b
DA-MO-WANG <zard20201116@gmail.com>
2023-05-23 16:47:00 +0800

    commit demo

---
+++ java/lambda/src/main/java/com/example/lambda/streamapi1/Demo.java
@@ -0,0 +1,8 @@
+package com.example.lambda.streamapi1;/**
+ * @Description TO DO
+ * @Classname Demo
+ * @Date 2023/5/23 16:46
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class Demo {
+}
