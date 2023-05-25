56891895bdc791615a113b5a9020a11ef51a1fde
DA-MO-WANG <zard20201116@gmail.com>
2023-05-25 16:33:00 +0800

    commit demo

---
+++ java/lambda/src/main/java/com/example/lambda/parelell/ParallelStreamBenchmark.java
@@ -0,0 +1,8 @@
+package com.example.lambda.parelell;/**
+ * @Description TO DO
+ * @Classname ParallelStreamBenchmark
+ * @Date 2023/5/25 16:32
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class ParallelStreamBenchmark {
+}
