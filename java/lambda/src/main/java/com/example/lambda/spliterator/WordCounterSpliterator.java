08aef74d7a570bc1f3e772b26bf5342ef1e1d079
DA-MO-WANG <zard20201116@gmail.com>
2023-05-26 15:35:01 +0800

    commit demo

---
+++ java/lambda/src/main/java/com/example/lambda/spliterator/WordCounterSpliterator.java
@@ -0,0 +1,8 @@
+package com.example.lambda.spliterator;/**
+ * @Description TO DO
+ * @Classname WordCounterSpliterator
+ * @Date 2023/5/26 15:34
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class WordCounterSpliterator {
+}
