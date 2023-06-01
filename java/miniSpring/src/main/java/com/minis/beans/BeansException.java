8187f978a709230f112291af24fc1d7506652e47
DA-MO-WANG <zard20201116@gmail.com>
2023-06-01 16:53:01 +0800

    commit demo

---
+++ java/miniSpring/src/main/java/com/minis/beans/BeansException.java
@@ -0,0 +1,8 @@
+package com.minis.beans;/**
+ * @Description TO DO
+ * @Classname BeansException
+ * @Date 2023/6/1 16:52
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class BeansException {
+}
