35cd5356c26d27148b8c9b5293f855a7e30950d4
DA-MO-WANG <zard20201116@gmail.com>
2023-06-01 17:01:00 +0800

    commit demo

---
+++ java/miniSpring/src/main/java/com/minis/beans/ClassPathXmlResource.java
@@ -0,0 +1,8 @@
+package com.minis.beans;/**
+ * @Description TO DO
+ * @Classname ClassPathXmlResource
+ * @Date 2023/6/1 17:00
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class ClassPathXmlResource {
+}
