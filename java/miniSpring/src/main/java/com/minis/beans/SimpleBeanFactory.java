7fa9077875819b647443fd7b726fa9abcee3a260
DA-MO-WANG <zard20201116@gmail.com>
2023-06-01 17:16:00 +0800

    commit demo

---
+++ java/miniSpring/src/main/java/com/minis/beans/SimpleBeanFactory.java
@@ -0,0 +1,8 @@
+package com.minis.beans;/**
+ * @Description TO DO
+ * @Classname SimpleBeanFactory
+ * @Date 2023/6/1 17:15
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class SimpleBeanFactory {
+}
