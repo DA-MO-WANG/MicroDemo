67a27b96e7d2b7808faacea6b6bdcfff1b2ae842
DA-MO-WANG <zard20201116@gmail.com>
2023-06-01 17:09:01 +0800

    commit demo

---
+++ java/miniSpring/src/main/java/com/minis/beans/XmlBeanDefinitionReader.java
@@ -0,0 +1,8 @@
+package com.minis.beans;/**
+ * @Description TO DO
+ * @Classname XmlBeanDefinitionReader
+ * @Date 2023/6/1 17:08
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class XmlBeanDefinitionReader {
+}
