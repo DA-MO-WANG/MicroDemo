b42a08399b1147cf1a66bfe472675e53a71e306f
DA-MO-WANG <zard20201116@gmail.com>
2023-06-01 16:28:00 +0800

    commit demo

---
+++ java/miniSpring/src/test/java/beans/AServiceImpl.java
@@ -0,0 +1,8 @@
+package beans;/**
+ * @Description TO DO
+ * @Classname AServiceImpl
+ * @Date 2023/6/1 16:27
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class AServiceImpl {
+}
---
+++ java/miniSpring/src/test/java/beans/BeanTest.java
@@ -0,0 +1,8 @@
+package beans;/**
+ * @Description TO DO
+ * @Classname BeanTest
+ * @Date 2023/6/1 16:27
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class BeanTest {
+}
