a85480efa882a9f7b83945fd944f9e4429869fa1
DA-MO-WANG <zard20201116@gmail.com>
2023-06-01 17:00:00 +0800

    commit demo

---
+++ java/miniSpring/src/main/java/com/minis/beans/Resource.java
@@ -0,0 +1,8 @@
+package com.minis.beans;/**
+ * @Description TO DO
+ * @Classname Resource
+ * @Date 2023/6/1 16:59
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public interface Resource {
+}
