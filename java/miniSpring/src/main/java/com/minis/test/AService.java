a4631bf95e72d331e1509c523cfca00b31d9e3fa
DA-MO-WANG <zard20201116@gmail.com>
2023-06-01 16:46:00 +0800

    commit demo

---
+++ java/miniSpring/src/main/java/com/minis/test/AService.java
@@ -0,0 +1,12 @@
+package beans;
+
+/**
+ * @Description TO DO
+ * @Classname AService
+ * @Date 2023/6/1 16:26
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */
+public interface AService {
+    void sayHello();
+}
