55fad1a39250d1857d7ed2cb92aeed9331997c4f
DA-MO-WANG <zard20201116@gmail.com>
2023-06-01 16:55:01 +0800

    commit demo

---
+++ java/miniSpring/src/main/java/com/minis/beans/BeanFactory.java
@@ -0,0 +1,8 @@
+package com.minis.beans;/**
+ * @Description TO DO
+ * @Classname BeanFactory
+ * @Date 2023/6/1 16:54
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public interface BeanFactory {
+}
