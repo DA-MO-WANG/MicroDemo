a58b68654f81e44889c3605288df300d153cbe0c
DA-MO-WANG <zard20201116@gmail.com>
2023-06-01 16:45:00 +0800

    commit demo

---
+++ java/miniSpring/src/main/java/com/minis/beans/AServiceImpl.java
@@ -0,0 +1,15 @@
+package beans;
+
+/**
+ * @Description TO DO
+ * @Classname AServiceImpl
+ * @Date 2023/6/1 16:27
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */
+public class AServiceImpl implements AService{
+    @Override
+    public void sayHello() {
+        System.out.println("a service 1 say hello.");
+    }
+}
