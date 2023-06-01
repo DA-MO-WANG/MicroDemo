53a35993427c1ed067d875eb867b4493caff935d
DA-MO-WANG <zard20201116@gmail.com>
2023-06-01 15:41:00 +0800

    commit demo

---
+++ java/miniSpring/src/main/java/com/minis/beans/BeanDefinition.java
@@ -0,0 +1,8 @@
+package com.minis.beans;/**
+ * @Description TO DO
+ * @Classname BeanDefinition
+ * @Date 2023/6/1 15:40
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class BeanDefinition {
+}
