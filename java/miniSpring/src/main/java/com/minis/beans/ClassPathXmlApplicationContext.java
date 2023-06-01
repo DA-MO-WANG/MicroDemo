7105787e99cf0a00760ef96de5a35cce63ae6169
DA-MO-WANG <zard20201116@gmail.com>
2023-06-01 15:46:01 +0800

    commit demo

---
+++ java/miniSpring/src/main/java/com/minis/beans/ClassPathXmlApplicationContext.java
@@ -0,0 +1,8 @@
+package com.minis.beans;/**
+ * @Description TO DO
+ * @Classname ClassPathXmlApplicationContext
+ * @Date 2023/6/1 15:45
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class ClassPathXmlApplicationContext {
+}
