41de77cb91bdef06ff44807cca63a8312bfa0063
DA-MO-WANG <zard20201116@gmail.com>
2023-06-02 10:33:00 +0800

    commit demo

---
+++ java/singletonPattern/src/main/java/version1/Test.java
@@ -0,0 +1,8 @@
+package version1;/**
+ * @Description TO DO
+ * @Classname Test
+ * @Date 2023/6/2 10:32
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class Test {
+}
