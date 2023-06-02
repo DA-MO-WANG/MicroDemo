468a3a687d1da34eb040b2abb73bccdf8976b377
DA-MO-WANG <zard20201116@gmail.com>
2023-06-02 14:26:01 +0800

    commit demo

---
+++ java/singletonPattern/src/main/java/version5/Singleton.java
@@ -0,0 +1,8 @@
+package version5;/**
+ * @Description TO DO
+ * @Classname Singleton
+ * @Date 2023/6/2 14:25
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public enum Singleton {
+}
