8866d4e86caae13c268f35d84bbcb993eaf5f77e
DA-MO-WANG <zard20201116@gmail.com>
2023-06-02 17:45:01 +0800

    commit demo

---
+++ java/miniSpring/src/main/java/com/minis/beans/SingletonBeanRegistry.java
@@ -0,0 +1,8 @@
+package com.minis.beans;/**
+ * @Description TO DO
+ * @Classname SingletonBeanRegistry
+ * @Date 2023/6/2 17:44
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public interface SingletonBeanRegistry {
+}
