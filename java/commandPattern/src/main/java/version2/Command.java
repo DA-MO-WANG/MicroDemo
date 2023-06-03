a5e336f74ed642ba5cba3e23e23cc0ae34294bba
DA-MO-WANG <zard20201116@gmail.com>
2023-06-03 15:28:00 +0800

    commit demo

---
+++ java/commandPattern/src/main/java/version2/Command.java
@@ -0,0 +1,8 @@
+package version2;/**
+ * @Description TO DO
+ * @Classname Command
+ * @Date 2023/6/3 15:27
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class Command {
+}
