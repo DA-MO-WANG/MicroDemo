77a016586e4b9e4e8ea25edfdcea280df0fd8ced
DA-MO-WANG <zard20201116@gmail.com>
2023-06-03 15:31:00 +0800

    commit demo

---
+++ java/commandPattern/src/main/java/version2/BakeMuttonCommand.java
@@ -0,0 +1,8 @@
+package version2;/**
+ * @Description TO DO
+ * @Classname BakeMuttonCommand
+ * @Date 2023/6/3 15:30
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class BakeMuttonCommand {
+}
