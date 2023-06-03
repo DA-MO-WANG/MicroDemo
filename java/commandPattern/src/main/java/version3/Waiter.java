12c8a78b438a2f13f54e533b3867a47c837725ed
DA-MO-WANG <zard20201116@gmail.com>
2023-06-03 15:40:01 +0800

    commit demo

---
+++ java/commandPattern/src/main/java/version3/Waiter.java
@@ -0,0 +1,8 @@
+package version3;/**
+ * @Description TO DO
+ * @Classname Waiter
+ * @Date 2023/6/3 15:39
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class Waiter {
+}
