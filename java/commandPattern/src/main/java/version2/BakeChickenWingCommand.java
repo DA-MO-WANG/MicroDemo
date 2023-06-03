e59ce64bf27a45eaf4b8d895f52bbd2a4168f12e
DA-MO-WANG <zard20201116@gmail.com>
2023-06-03 15:33:00 +0800

    commit demo

---
+++ java/commandPattern/src/main/java/version2/BakeChickenWingCommand.java
@@ -0,0 +1,8 @@
+package version2;/**
+ * @Description TO DO
+ * @Classname BakeChickenWingCommand
+ * @Date 2023/6/3 15:32
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class BakeChickenWingCommand {
+}
