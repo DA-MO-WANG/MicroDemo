ccb8e0b12e17d014113beff3c76c6cb6b3d6bc17
DA-MO-WANG <zard20201116@gmail.com>
2023-06-03 15:25:00 +0800

    commit demo

---
+++ java/commandPattern/src/main/java/version1/Barbecuer.java
@@ -0,0 +1,8 @@
+package version1;/**
+ * @Description TO DO
+ * @Classname Barbecuer
+ * @Date 2023/6/3 15:24
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class Barbecuer {
+}
