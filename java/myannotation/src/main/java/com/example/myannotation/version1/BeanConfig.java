0151b9608a01f4363e6eaa22bd4c5c6c659ccbc5
DA-MO-WANG <zard20201116@gmail.com>
2023-06-04 11:37:00 +0800

    commit demo

---
+++ java/myannotation/src/main/java/com/example/myannotation/version1/BeanConfig.java
@@ -0,0 +1,8 @@
+package com.example.myannotation.version1;/**
+ * @Description TO DO
+ * @Classname BeanConfig
+ * @Date 2023/6/4 11:36
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class BeanConfig {
+}
