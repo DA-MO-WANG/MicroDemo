62cd07a4d158268712bc0034016efa10b6145fd8
DA-MO-WANG <zard20201116@gmail.com>
2023-06-04 11:35:00 +0800

    commit demo

---
+++ java/myannotation/src/main/java/com/example/myannotation/version1/Cold.java
@@ -0,0 +1,8 @@
+package com.example.myannotation.version1;/**
+ * @Description TO DO
+ * @Classname Cold
+ * @Date 2023/6/4 11:34
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public @interface Cold {
+}
