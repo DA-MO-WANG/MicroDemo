e061366dd13e220e2f77bdacdf549c69b9cc9df2
DA-MO-WANG <zard20201116@gmail.com>
2023-06-04 14:33:01 +0800

    commit demo

---
+++ java/chainofresponsibility/src/main/java/version1/ConcreteHandler2.java
@@ -0,0 +1,8 @@
+package version1;/**
+ * @Description TO DO
+ * @Classname ConcreteHandler2
+ * @Date 2023/6/4 14:32
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class ConcreteHandler2 {
+}
