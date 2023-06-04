68c9f06d216c53f83eac03cf61a6d7924503f035
DA-MO-WANG <zard20201116@gmail.com>
2023-06-04 11:36:00 +0800

    commit demo

---
+++ java/myannotation/src/main/java/com/example/myannotation/version1/IceCream.java
@@ -0,0 +1,8 @@
+package com.example.myannotation.version1;/**
+ * @Description TO DO
+ * @Classname IceCream
+ * @Date 2023/6/4 11:35
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class IceCream {
+}
