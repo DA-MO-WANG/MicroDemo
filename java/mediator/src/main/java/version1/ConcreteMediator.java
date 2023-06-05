4617680250c59e9e9f78b7cd2f19e950d61c1076
DA-MO-WANG <zard20201116@gmail.com>
2023-06-05 16:23:00 +0800

    commit demo

---
+++ java/mediator/src/main/java/version1/ConcreteMediator.java
@@ -0,0 +1,8 @@
+package version1;/**
+ * @Description TO DO
+ * @Classname ConcreteMediator
+ * @Date 2023/6/5 16:22
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class ConcreteMediator {
+}
