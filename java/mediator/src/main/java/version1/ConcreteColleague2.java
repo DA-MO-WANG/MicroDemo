8035a4434554bfda6865dafed1069eec934b2fce
DA-MO-WANG <zard20201116@gmail.com>
2023-06-05 16:28:01 +0800

    commit demo

---
+++ java/mediator/src/main/java/version1/ConcreteColleague2.java
@@ -0,0 +1,8 @@
+package version1;/**
+ * @Description TO DO
+ * @Classname ConcreteColleague2
+ * @Date 2023/6/5 16:27
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class ConcreteColleague2 {
+}
