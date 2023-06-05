454cd574aecead01a06eb35a77596bf1db9e937a
DA-MO-WANG <zard20201116@gmail.com>
2023-06-05 16:34:00 +0800

    commit demo

---
+++ java/mediator/src/main/java/version1/Client.java
@@ -0,0 +1,8 @@
+package version1;/**
+ * @Description TO DO
+ * @Classname Client
+ * @Date 2023/6/5 16:33
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class Client {
+}
